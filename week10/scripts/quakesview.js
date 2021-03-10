da5c840df7ad6524f05f52a3c7823d9dee3e1d73
trvrpp71 <38666260+trvrpp71@users.noreply.github.com>
2021-03-10 16:26:52 -0600

    a

--- week10/scripts/quakesview.js
+++ week10/scripts/quakesview.js
@@ -32,4 +32,4 @@ export default class QuakesView {
         .joint('');
 
     }
-  }
+  } 
--- week10/scripts/utilities.js
+++ week10/scripts/utilities.js
@@ -1,19 +1,22 @@
-export function getJSON(url){
-    fetch(url).then((response) => {
-        console.log('resolved', response);
-        return response.json();
-    }).then((data) => {
-        console.log(data)
-    }).catch((err) => {
-        console.log('rejected', err);
+export function getJSON(url) {
+    return fetch(url)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        } else {
+          //console.log(response.json());
+          return response.json();
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+  export const getLocation = function(options) {
+    return new Promise(function(resolve, reject) {
+      navigator.geolocation.getCurrentPosition(resolve, reject, options);
     });
-};
-
-export const getLocation = function(options){
-    return new Promise(function(resolve,reject){
-        navigator.geolocation.getCurrentPosition(resolve,reject,options)
-    });
-};
+  };
 
 // export function getLocation(){
 //     let options = {timeout:60000};
