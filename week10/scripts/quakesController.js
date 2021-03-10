91f997345fa4984fcd478fd0e75021fe7e5f14ba
trvrpp71 <38666260+trvrpp71@users.noreply.github.com>
2021-03-10 12:42:23 -0600

    daf

--- week10/scripts/quakesController.js
+++ week10/scripts/quakesController.js
@@ -1,6 +1,6 @@
-import {getLocation} from './utilities.js';
-import Quake from './Quake.js';
-import QuakesView from './QuakesView.js';
+import {getLocation} from './utilities.js'
+import Quake from './quake.js';
+import QuakesView from './quakesview.js';
 
 // Quake controller
 export default class QuakesController {
